97d514c055ba385d0368b46cb04951dea7c33a30
Alabê Duarte <alabeduarte@gmail.com>
2011-11-17 21:11:50 -0300

    ...

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,7 +2,12 @@ class Task
   include MongoMapper::Document
 
   key :description, String
+  key :owner_id, ObjectId
   
-  belongs_to :owner, :class => "User"
+  belongs_to :owner, :class => 'User'
+  
+  def owner
+    User.find(@owner_id)
+  end
 
 end
